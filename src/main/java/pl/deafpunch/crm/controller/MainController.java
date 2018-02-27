ec6c7aba7a72eea09ccd6068db359c9050681f99
Bartosz <bartosz.beli@gmail.com>
2018-02-27 15:15:46 +0100

    update

--- src/main/java/pl/deafpunch/crm/controller/MainController.java
+++ src/main/java/pl/deafpunch/crm/controller/MainController.java
@@ -3,12 +3,18 @@ package pl.deafpunch.crm.controller;
 import java.security.Principal;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import pl.deafpunch.crm.entity.User;
 import pl.deafpunch.crm.repository.UserRepository;
@@ -27,9 +33,19 @@ public class MainController {
 	 *  @return Login page view
 	 */
 	
-	@GetMapping(path="/login")
+	@GetMapping(path= {"/login", "/"})
 	public String getLandingPage() {
 		return "login";	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "login";
+	}
+	
 
 	/**
 	 *  Shows main dashboard for logged in user
@@ -41,18 +57,18 @@ public class MainController {
 	 * @return dashboard view
 	 */
 	
-	@GetMapping(path="/dashboard")
+	@GetMapping(path="/index")
 	public String index(Principal principal, HttpServletRequest request, Model model, HttpSession sess) {
 		if (sess.getAttribute("userID") == null) {
 			User user = userServiceImpl.findByUsername(principal.getName());
 			request.getSession().setAttribute("userID", user.getId());
 			model.addAttribute("loggedInUser", user.getUsername());
-			return "dashboard";
+			return "index";
 		}else {			
 			Long idFromSession = (Long) sess.getAttribute("userID");
 			User user = userRepository.findById(idFromSession);
 			model.addAttribute("loggedInUser", user.getUsername());
-			return "dashboard";
+			return "index";
 		}
 		
 	}
